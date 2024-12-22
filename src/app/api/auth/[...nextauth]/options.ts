9f14bd0d1fd2560017cbfde3b976c0686afab378
Akhil092004 <akhiljain092004@gmail.com>
2024-12-22 22:04:59 +0530

    implement authentication flow with NextAuth and create sign-in page

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextAuthOptions } from "next-auth";
 import  CredentialsProvider  from "next-auth/providers/credentials";
@@ -37,19 +38,46 @@ export const authOptions : NextAuthOptions = {
                     const isPasswordCorrect = await bcrypt.compare(credentials.password,user.password)
 
                     if(!isPasswordCorrect){
-                        throw new Error("Incorrect Password")
+                        // throw new Error("Incorrect Password")
+                        throw new Error("Incorrect Password")  
                     }
                     else{
                         return user
                     }
 
-
                 } catch (err:any) {
                     throw new Error(err)
                 }
 
-
             }
         })
-    ]
+    ],
+    callbacks:{
+        async jwt({token, user}){
+            if(user){
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified
+                token.isAcceptingMessages = user.isAcceptingMessages
+                token.username = user.username
+            }
+            return token
+        },
+        async session({session, token}){
+            if(token){
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+
+            }
+            return session
+        }
+    },
+    pages:{
+        signIn:'/sign-in',
+    },
+    session:{
+        strategy:"jwt"
+    },
+    secret:process.env.NEXTAUTH_SECRET,
 }
